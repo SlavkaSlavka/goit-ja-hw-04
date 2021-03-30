ab401eacbdfa6a4cf6b8cd1aae2489204c47210f
SlavkaSlavka <56847146+SlavkaSlavka@users.noreply.github.com>
2021-03-30 16:25:20 +0300

    task-1

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,24 +1,25 @@
-//Напиши скрипт, который, для объекта user, последовательно:
-
-//добавляет поле mood со значением 'happy'
-//заменяет значение hobby на 'skydiving'
-//заменяет значение premium на false
-//выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
-
-
-
-const user = {
-  name: 'Mango',
-  age: 20,
-  hobby: 'html',
-  premium: true,
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
 };
 
-user.mood = 'happy';
-user.hobby = 'skydiving'
-user.premium = !user.premium;
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-const keys = Object.keys(user);
-for (const key of keys) {
-  console.log( `${key} :  ${user[key]}`);
-}
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
